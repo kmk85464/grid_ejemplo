f9dc5a3fb11a5b83159c85b49e11120815ebc9e6
Roberto Calvo <KMK85464@educastur.es>
2025-01-10 20:46:07 +0100

    Fix

--- App.js
+++ App.js
@@ -6,28 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Stack from 'react-bootstrap/Stack';
 function App() {
   return (
-    <>
-        <Container>
+    
+    <Container>
       <Row>
-        <Col>
-          <div class="sm:m-1 md:m-5 lg:m-10" col xs={12} sm={12} md={6} offset={1}  style={{backgroundImage: "url('./Mujer.jpeg')",  height: '240px',backgroundSize: 'cover', backgroundPosition: 'center center'}}>Layout Mujer</div>
-        </Col>
-        <Col>
-        <div class="sm:m-1 md:m-5 lg:m-10" col xs={12} sm={12} md={6} offset={5}   style={{backgroundImage: "url('./Hombre.jpeg')", height: '240px', backgroundSize: 'cover', backgroundPosition: 'center center'}}>Layout Hombre</div>
-
-        </Col>
-        <Col>
-        <div  class="sm:m-1 md:m-5 lg:m-10"  col xs={12} sm={12} md={6} offset={5}  style={{backgroundImage: "url('./Niño.jpeg')", height: '240px', backgroundSize: 'cover', backgroundPosition: 'center center'}}>Layout Niño</div>
-
-        </Col>
+        <Col md={6} lg={4}   style={{backgroundImage: "url('./Mujer.jpeg')",  height: '240px',backgroundSize: 'cover', backgroundPosition: 'center center'}}Layout Mujer></Col>
+        <Col md={6} lg={4} style={{backgroundImage: "url('./Hombre.jpeg')", height: '240px', backgroundSize: 'cover', backgroundPosition: 'center center'}}Layout Hombre></Col>
+        <Col  col  md={6}lg={4}  style={{backgroundImage: "url('./Niño.jpeg')", height: '240px', backgroundSize: 'cover', backgroundPosition: 'center center'}}Layout Niño></Col>
+        
       </Row>
     </Container>
-  
-    </>
+   
   );
-
-
-  
 }
 
 export default App;
